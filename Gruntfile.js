9cd0fc0f3aeba8bb107298694de3fd7a3491be69
Samuel Reed <sam@tixelated.com>
2013-10-19 14:37:08 +0800

    Add gruntfile, etc

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+/* jshint node: true */
+var matchdep = require('matchdep');
+
+module.exports = function(grunt) {
+  "use strict";
+
+  // Project configuration.
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          port: 3000,
+          base: '.',
+          open: 'http://localhost:3000/elements/examples/index.html'
+        }
+      }
+    },
+  });
+
+  matchdep.filterAll('grunt-*').forEach(function(pkgName){
+    grunt.loadNpmTasks(pkgName);
+  });
+};
